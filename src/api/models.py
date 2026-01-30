4b67c8213b4b8cca81399ddb15ef67224c74a399
David Alvarez Iglesias <daalvarezig@gmail.com>
2026-01-30 18:29:30 +0000

    Update models and routes

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,18 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Client(db.Model):
+    __tablename__= "client"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+
+    def.serialize(self):
+        return{
+            "id": self.id,
+            "email" : self.email,
+            "is_active" : self.is_active
+        }
+
