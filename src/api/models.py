8c2f45698b12b494f95cdcaedebaca78596c657a
Sergio Cordoba <sergiocordoba246@gmail.com>
2026-01-31 13:17:50 +0000

    Administrador model add

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,19 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Amdministrador(db.Model):
+    __tablename__ = "Amdministrador"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }       
