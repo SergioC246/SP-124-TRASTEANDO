ea78adab639c295411edb076a65fa167baba6501
David Alvarez Iglesias <daalvarezig@gmail.com>
2026-02-02 15:18:19 +0000

    Added. jsx

---
+++ src/front/pages/ClientDetails.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { getClients } from "../utilsClients.js";
+
+
+export const ClientsDetail = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [client, setClient] = useState(null);
+
+    useEffect(() => {
+        getClients(id)
+            .then(setClient)
+            .catch(console.error);
+    }, [id]);
+
+    if (!client) return <p>Loading client...</p>;
+
+    return (
+        <div className="container mt-4">
+            <h1>Client Details</h1>
+         
+        <ul className="list-group mb-3">
+            <li className="list-group-item">
+               <strong>ID:</strong> {client.id}
+            </li>
+            <li className="list-group-item">
+               <strong>Email:</strong> {client.email}
+            </li>
+            <li className="list-group-item">
+               <strong>Status:</strong> {""}
+               {client.is_active ? "Active" : "Inactive"}
+            </li>
+        </ul>
+
+        <button className="btn btn-secondary"
+                onClick={() => navigate("/clients")}
+         >
+            Back
+         </button>
+    </div > 
+   );
+  };
--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -15,7 +15,8 @@ import { Companies } from "./pages/Companies";
 import { CreateCompanies } from "./pages/CreateCompanies";
 import { CompanieDetails } from "./pages/CompanieDetails";
 import { AdminUserDetails } from "./pages/AdminUserDetails";
-
+import { Clients } from "./pages/Clients";
+import { ClientDetails } from "./pages/ClientDetails";
 
 
 
@@ -36,6 +37,32 @@ export const router = createBrowserRouter(
       <Route path="/companies" element={<Companies />} />
       <Route path="/createCompanies" element={<CreateCompanies />} />
       <Route path="/companies/:id" element={<CompanieDetails />} />
+      <Route path="/clients" element={<ClientsList />} />
+      <Route path="/clients" element={<ClientsCreate />} />
+      <Route path="/clients" element={<ClientsDetails />} />
+      <Route path="/clients" element={<ClientsEdit />} />
     </Route>
   )
 )
+
+
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    // CreateRoutesFromElements function allows you to build route elements declaratively.
+    // Create your routes here, if you want to keep the Navbar and Footer in all views, add your new routes inside the containing Route.
+    // Root, on the contrary, create a sister Route, if you have doubts, try it!
+    // Note: keep in mind that errorElement will be the default page when you don't get a route, customize that page to make your project more attractive.
+    // Note: The child paths of the Layout element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
+
+    // Root Route: All navigation will start from here.
+    <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
+
+      {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
+      <Route path="/" element={<Home />} />
+      <Route path="/single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
+      <Route path="/demo" element={<Demo />} />
+      {/* <Route path="/clients/:id" element={<Client />} /> */}
+    </Route>
+  )
+);
