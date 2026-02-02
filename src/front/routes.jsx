242ce0662d0ff26628e5735d4ee99281ca2cae8c
David Alvarez Iglesias <daalvarezig@gmail.com>
2026-02-02 16:34:03 +0000

    Added all .jsx

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -10,13 +10,15 @@ import { Home } from "./pages/Home";
 import { AdminUsers } from "./pages/AdminUsers"
 import { AdminUserCreate } from "./pages/AdminUserCreate";
 import { AdminUserEdit } from "./pages/AdminUserEdit";
-import { Clients } from "./pages/Clients";
 import { Companies } from "./pages/Companies";
 import { CreateCompanies } from "./pages/CreateCompanies";
 import { CompanieDetails } from "./pages/CompanieDetails";
 import { AdminUserDetails } from "./pages/AdminUserDetails";
 import { Clients } from "./pages/Clients";
+import { ClientsList } from "./pages/ClientsList";
+import { ClientCreate } from "./pages/ClientCreate";
 import { ClientDetails } from "./pages/ClientDetails";
+import { ClientEdit } from "./pages/ClientEdit";
 
 
 
@@ -31,24 +33,19 @@ export const router = createBrowserRouter(
       <Route path="/admin-details/:id" element={<AdminUserDetails />} />
 
       {/* Rutas client */}
-      <Route path="/clients" element={<Clients />} />
 
       {/* Rutas company */}
       <Route path="/companies" element={<Companies />} />
       <Route path="/createCompanies" element={<CreateCompanies />} />
       <Route path="/companies/:id" element={<CompanieDetails />} />
       <Route path="/clients" element={<ClientsList />} />
-      <Route path="/clients" element={<ClientsCreate />} />
-      <Route path="/clients" element={<ClientsDetails />} />
-      <Route path="/clients" element={<ClientsEdit />} />
-    </Route>
-  )
-)
+      <Route path="/clients" element={<ClientCreate />} />
+      <Route path="/clients" element={<ClientDetails />} />
+      <Route path="/clients" element={<ClientEdit />} />
+  
+ )
+);
 
-
-
-export const router = createBrowserRouter(
-  createRoutesFromElements(
     // CreateRoutesFromElements function allows you to build route elements declaratively.
     // Create your routes here, if you want to keep the Navbar and Footer in all views, add your new routes inside the containing Route.
     // Root, on the contrary, create a sister Route, if you have doubts, try it!
@@ -56,13 +53,11 @@ export const router = createBrowserRouter(
     // Note: The child paths of the Layout element replace the Outlet component with the elements contained in the "element" attribute of these child paths.
 
     // Root Route: All navigation will start from here.
-    <Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >
+    {/*<Route path="/" element={<Layout />} errorElement={<h1>Not found!</h1>} >*/}
 
       {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
-      <Route path="/" element={<Home />} />
-      <Route path="/single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
-      <Route path="/demo" element={<Demo />} />
-      {/* <Route path="/clients/:id" element={<Client />} /> */}
+      {/*<Route path="/" element={<Home />} />*/}
+
     </Route>
   )
 );
