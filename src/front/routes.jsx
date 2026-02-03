2324643333650c1239aae8436a2ce4887df719f8
David Alvarez Iglesias <daalvarezig@gmail.com>
2026-02-03 19:01:50 +0000

    Added StorageEdit.jsx

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -10,11 +10,18 @@ import { Home } from "./pages/Home";
 import { AdminUsers } from "./pages/AdminUsers"
 import { AdminUserCreate } from "./pages/AdminUserCreate";
 import { AdminUserEdit } from "./pages/AdminUserEdit";
-import { Clients } from "./pages/Clients";
 import { Companies } from "./pages/Companies";
 import { CreateCompanies } from "./pages/CreateCompanies";
 import { CompanieDetails } from "./pages/CompanieDetails";
 import { AdminUserDetails } from "./pages/AdminUserDetails";
+import { ClientList } from "./pages/ClientList";
+import { ClientCreate } from "./pages/ClientCreate";
+import { ClientDetails } from "./pages/ClientDetails";
+import { ClientEdit } from "./pages/ClientEdit";
+import { StorageList } from "./pages/StorageList";
+import { StorageCreate } from "./pages/StorageCreate";
+import { StorageDetails } from "./pages/StorageDetails";
+import { StorageEdit } from "./pages/StorageEdit";
 
 
 
@@ -36,6 +43,13 @@ export const router = createBrowserRouter(
       <Route path="/companies" element={<Companies />} />
       <Route path="/createCompanies" element={<CreateCompanies />} />
       <Route path="/companies/:id" element={<CompanieDetails />} />
+
+      {/* Rutas storage */}
+      <Route path="/storages" element={<StorageList />} />
+      <Route path="/storages/new" element={<StorageCreate />} />
+      <Route path="/storages/:id" element={<StorageDetails />} />
+      <Route path="/storages/:id/edit" element={<StorageEdit />} />
+
     </Route>
   )
 )
