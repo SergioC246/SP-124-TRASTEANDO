8bb9f4821126f67a68946540ae1215c872ed13a9
David Alvarez Iglesias <daalvarezig@gmail.com>
2026-02-27 11:14:29 +0000

    fix errors

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -86,7 +86,7 @@ export const router = createBrowserRouter(
 
       {/* Rutas client */}
 
-      <Route path="/clients-edit/:id/edit" element={<ClientEdit />} />
+
       <Route path="/client/login" element={<ClientLogin />} />
       <Route path="/client/signup" element={<ClientSignup />} />
 
