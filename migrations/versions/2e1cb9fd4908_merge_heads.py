b7586e621293b0cab804f89a195903b260a5a421
Irene <isanarmu@gmail.com>
2026-03-02 16:09:52 +0000

    merge alembic heads

---
+++ migrations/versions/2e1cb9fd4908_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 2e1cb9fd4908
+Revises: 382de8c756be, 408687cacba5
+Create Date: 2026-03-02 16:09:26.083529
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2e1cb9fd4908'
+down_revision = ('382de8c756be', '408687cacba5')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
